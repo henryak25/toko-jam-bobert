6eb0dd15794866603fa9c8803d22b897cd406cde
henryak25 <henryadityakosasi@gmail.com>
2024-09-17 11:26:45 +0700

    Tugas 03

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.db import models
-
+import uuid
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     description = models.TextField()
