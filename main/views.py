cc3460f0e5c8788abba7cedf4b1688f158351480
henryak25 <henryadityakosasi@gmail.com>
2024-10-02 11:16:52 +0700

    Tugas 05

--- main/views.py
+++ main/views.py
@@ -4,15 +4,20 @@ from main.models import Product
 from main.forms import ProductForm
 from django.core import serializers
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+
+
 @login_required(login_url='/login')
 def show_main(request):
     product_entries = Product.objects.filter(user=request.user)
     context = {
+        'nama_mahasiswa' : 'Henry Aditya Kosasi',
+        'npm': 2306124990,
+        'class': 'PBP F',
         'name': request.user.username,
         'description' : 'jujujuujujuupiter',
         'price': 50,
@@ -83,3 +88,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    # Get product berdasarkan id
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get product berdasarkan id
+    product = Product.objects.get(pk = id)
+    # Hapus product
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
