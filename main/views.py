c0246d165c305840e9a317a2eea6b89794d64c2d
henryak25 <henryadityakosasi@gmail.com>
2024-10-09 09:48:51 +0700

    hehe

--- main/views.py
+++ main/views.py
@@ -9,11 +9,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
-
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
     context = {
         'nama_mahasiswa' : 'Henry Aditya Kosasi',
         'npm': 2306124990,
@@ -21,7 +22,6 @@ def show_main(request):
         'name': request.user.username,
         'description' : 'jujujuujujuupiter',
         'price': 50,
-        'product_entries': product_entries,
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "main.html", context)
@@ -40,11 +40,11 @@ def create_product_entry(request):
     return render(request, "create_product_entry.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -111,3 +111,20 @@ def delete_product(request, id):
     product.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    description = strip_tags(request.POST.get("description"))
+    price = request.POST.get("price")
+    user = request.user
+
+    new_product = Product(
+        name=name, description = description,
+        price = price,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
